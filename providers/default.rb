8128ac0eb91f6dfa2461f598a9be8493cae28edf
Lopaka Delp <lopaka@rightscale.com>
2014-02-18 00:46:37 -0800

    Updated create and destroy actions to only use specific cloud actions when needed

--- providers/default.rb
+++ providers/default.rb
@@ -5,82 +5,44 @@ def load_current_resource
 end
 
 action :create do
-  if new_resource.provider == "dnsmadeeasy" || new_resource.provider == "aws"
-    # using Fog DNSMadeEasy API calls,
-    # http://rubydoc.info/gems/fog/Fog/DNS/DNSMadeEasy/Real
 
-    # Initiate connection and grab list of domains in account
-#    account_domains = connection.list_domains
+  # Use FQDN to generate domain.
+  # Will assume domain name is the removal of the first section of FQDN.
+  (subdomain, domain) = new_resource.entry_name.split('.', 2)
 
-
-    # Use FQDN to generate domain.
-    # Will assume domain name is the removal of the first section of FQDN.
-    (subdomain, domain) = new_resource.entry_name.split('.', 2)
-
-    # Verify domain is in account
-#-----------
-#    raise "DEBUG blah: #{connection.zones.inspect}"
-    # DME - zone.id matches domain
-    # AWS - zone.domain matches domain
-    unless zone = connection.zones.detect { | zone_entry | zone_entry.id =~ /^#{domain}\.{0,1}$/ || zone_entry.domain =~ /^#{domain}\.{0,1}$/ }
-      raise "DOMAIN '#{domain}' NOT AVAILABLE IN ACCOUNT"
-    end
-#-----------
-
-###    record = zone.records.detect do |r|
-###      r.name == new_resource.entry_name
-###    end
-###    raise "DEBUG blah1: #{zone.records.inspect}"
-
-    # DME record.name matches subdomain
-    # AWS record.name matches FQDN ending in .
-    if record = zone.records.detect { | record_entry | record_entry.name == subdomain || record_entry.name =~ /^#{subdomain}\.#{domain}\.{0,1}$/ } 
-      raise "Entry '#{subdomain}.#{domain}' already exists"
-    end
-
-    # Check all A records if DNS entry already exists.
-#    all_records = connection.list_records(domain,{"type" => "A"})
-#    if all_records[:body].detect { | entry | entry['name'] == subdomain }
-#      raise "Entry '#{subdomain}.#{domain}' already exists"
-#    end
-
-    options = Hash.new
-    options[:ttl] = new_resource.ttl ? new_resource.ttl : 1800
-
-    connection.create_record(domain, subdomain, new_resource.type.upcase, new_resource.entry_value, options)
-
-    Chef::Log.info "Created DNS entry: #{new_resource.entry_name} -> #{new_resource.entry_value}"
-
-    new_resource.updated_by_last_action(true)
-  else
-    zone = connection.zones.detect do |z|
-      z.domain == new_resource.domain
-    end
-    record = zone.records.detect do |r|
-      r.name == new_resource.entry_name
-    end
-    args = Mash.new(
-      :value => new_resource.entry_value,
-      :name => new_resource.entry_name,
-      :type => new_resource.type.upcase
-    )
-    args[:ttl] = new_resource.ttl if new_resource.ttl
-    args[:priority] = new_resource.priority if new_resource.priority
-    if(record)
-      diff = args.keys.find_all do |k|
-        record.send(k) != args[k]
-      end
-      unless(diff.empty?)
-        record.update(args)
-        Chef::Log.info "Updated DNS entry: #{new_resource.entry_name} -> #{diff.map{|k| "#{k}:#{args[k]}"}.join(', ')}"
-        new_resource.updated_by_last_action(true)
-      end
-    else
-      zone.records.create(args)
-      Chef::Log.info "Created DNS entry: #{new_resource.entry_name} -> #{new_resource.entry_value}"
-      new_resource.updated_by_last_action(true)
-    end
+  # Verify domain is in account
+  # DME - zone.id matches domain
+  # AWS - zone.domain matches domain
+  unless zone = connection.zones.detect { | zone_entry | zone_entry.id =~ /^#{domain}\.{0,1}$/ || zone_entry.domain =~ /^#{domain}\.{0,1}$/ }
+    raise "DOMAIN '#{domain}' NOT AVAILABLE IN ACCOUNT"
   end
+
+  # Check records if DNS entry already exists.
+  # DME record.name matches subdomain ie 'www'
+  # AWS record.name matches FQDN ending in 'www.domain.com.'
+  if record = zone.records.detect { | record_entry | record_entry.name == subdomain || record_entry.name =~ /^#{subdomain}\.#{domain}\.{0,1}$/ } 
+    raise "Entry '#{subdomain}.#{domain}' already exists"
+  end
+
+  # Create hash with options for creating the record.
+  options = Hash.new
+  options[:ttl] = new_resource.ttl ? new_resource.ttl : 1800
+  options[:priority] = new_resource.priority if new_resource.priority
+
+  case new_resource.provider
+  when "dnsmadeeasy"
+    # Using Fog DNSMadeEasy API call:
+    # http://rubydoc.info/gems/fog/Fog/DNS/DNSMadeEasy/Real
+    connection.create_record(domain, subdomain, new_resource.type.upcase, new_resource.entry_value, options)
+  else
+    zone.records.create({:value => new_resource.entry_value,
+                        :name => new_resource.entry_name,
+                        :type => new_resource.type.upcase}.merge(options)
+                       )
+  end
+
+  Chef::Log.info "Created DNS entry: #{new_resource.entry_name} -> #{new_resource.entry_value}"
+  new_resource.updated_by_last_action(true)
 end
 
 action :update do
@@ -102,41 +64,33 @@ action :update do
 end
 
 action :destroy do
-  if new_resource.provider == "dnsmadeeasy"
-    # Initiate connection and grab list of domains in account
-    account_domains = connection.list_domains
+  # Use FQDN to generate domain.
+  # Will assume domain name is the removal of the first section of FQDN.
+  (subdomain, domain) = new_resource.entry_name.split('.', 2)
 
-    # Use FQDN to generate domain.
-    # Will assume domain name is the removal of the first section of FQDN.
-    (subdomain, domain) = new_resource.entry_name.split('.', 2)
-
-    # Verify domain is in account
-    raise "DOMAIN '#{domain}' NOT AVAILABLE IN ACCOUNT" unless account_domains[:body]["list"].include?(domain)
-
-    # Check all A records if DNS entry already exists.
-    all_records = connection.list_records(domain,{"type" => "A"})
-    unless found_record = all_records[:body].find { | entry | entry['name'] == subdomain }
-      raise "Entry '#{new_resource.entry_name}' does not exist"
-    end
-
-    connection.delete_record(domain, found_record['id'])
-    Chef::Log.info "Deleted DNS entry #{new_resource.entry_name} (#{found_record['id']})"
-
-    new_resource.updated_by_last_action(true)
-
-  else
-    zone = connection.zones.detect do |z|
-      z.domain == new_resource.domain
-    end
-    record = zone.records.detect do |r|
-      r.name == new_resource.entry_name
-    end
-    if(record)
-      record.destroy
-      Chef::Log.info "Destroying DNS entry: #{new_resource.entry_name}"
-      new_resource.updated_by_last_action(true)
-    end
+  # Verify domain is in account
+  # DME - zone.id matches domain
+  # AWS - zone.domain matches domain
+  unless zone = connection.zones.detect { | zone_entry | zone_entry.id =~ /^#{domain}\.{0,1}$/ || zone_entry.domain =~ /^#{domain}\.{0,1}$/ }
+    raise "DOMAIN '#{domain}' NOT AVAILABLE IN ACCOUNT"
   end
+
+  # Check records if DNS entry already exists.
+  # DME record.name matches subdomain ie 'www'
+  # AWS record.name matches FQDN ending in 'www.domain.com.'
+  unless record = zone.records.detect { | record_entry | record_entry.name == subdomain || record_entry.name =~ /^#{subdomain}\.#{domain}\.{0,1}$/ } 
+    raise "Entry '#{new_resource.entry_name}' does not exist"
+  end
+
+  case new_resource.provider
+  when "dnsmadeeasy"
+    connection.delete_record(domain, record.id)
+  else
+    record.destroy
+  end
+
+  Chef::Log.info "Deleted DNS entry #{new_resource.entry_name}"
+  new_resource.updated_by_last_action(true)
 end
 
 def connection
