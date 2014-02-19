59cfc7f274f88ca4fdfe872606f6f7acaa6b8c28
Lopaka Delp <lopaka@rightscale.com>
2014-02-19 10:51:09 -0800

    Set update recipe with oldvalue attribute

--- recipes/update.rb
+++ recipes/update.rb
@@ -3,6 +3,7 @@ include_recipe 'dns::fog'
 dns 'Update DNS entry' do
   entry_name lazy{ node[:dns][:entry][:name] }
   entry_value node[:dns][:entry][:value]
+  entry_oldvalue node[:dns][:entry][:oldvalue]
   type node[:dns][:entry][:type]
   ttl node[:dns][:entry][:ttl]
   action :update
