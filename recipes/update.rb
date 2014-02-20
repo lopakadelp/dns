7eeb903ff37848ca8cf4fff8f62e4f9cc45b5007
Lopaka Delp <lopaka@rightscale.com>
2014-02-20 11:59:34 -0800

    Simple coding standard fix.

--- recipes/update.rb
+++ recipes/update.rb
@@ -1,7 +1,7 @@
 include_recipe 'dns::fog'
 
 dns 'Update DNS entry' do
-  entry_name lazy{ node[:dns][:entry][:name] }
+  entry_name lazy { node[:dns][:entry][:name] }
   entry_value node[:dns][:entry][:value]
   entry_oldvalue node[:dns][:entry][:oldvalue]
   type node[:dns][:entry][:type]
